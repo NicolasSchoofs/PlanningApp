5b6d01ba5e9799e237aefe8f327a0e681af949d3
Nicolas Schoofs <s130014@ap.be>
2023-10-30 00:59:21 +0100

    working on delete event

--- frontend/app/components/calendar.tsx
+++ frontend/app/components/calendar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React from "react";
 import {
   Box,
   Grid,
@@ -8,12 +9,20 @@ import {
   Text,
   Center,
   Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { Event } from "./interfaces";
 
 import axios from "axios";
-
 
 async function fetchData(): Promise<Event[]> {
   try {
@@ -29,6 +38,19 @@ async function fetchData(): Promise<Event[]> {
   } catch (error) {
     console.error("Error fetching data:", error);
     return []; // Return an empty array in case of an error
+  }
+}
+async function deleteEvent(id: number) {
+  try {
+    const response = await axios.delete("http://localhost:8080/events/" + id);
+
+    if (response.status !== 200) {
+      throw new Error("Network response was not ok");
+    }
+
+    console.log(response);
+  } catch (error) {
+    console.error("Error fetching data:", error);
   }
 }
 function getCurrentWeekDates() {
@@ -55,120 +77,171 @@ export default function Calendar() {
   const bgWeekDay = useColorModeValue("gray.200", "gray.900");
   const itemCount = 4;
   const [events, setEvents] = useState<Event[]>([]);
+
+  const [openModalIndex, setOpenModalIndex] = useState(null);
+
+  const openModal = (index: any) => {
+    setOpenModalIndex(index);
+  };
+
+  const closeModal = () => {
+    setOpenModalIndex(null);
+  };
+
+  const handleDeleteClick = (id: number, e) => {
+    deleteEvent(id)
+      .then(() => {
+
+        })
+      .catch((error) => {
+        console.error("Error deleting event:", error);
+      });
+  };
+
   useEffect(() => {
-  async function fetchAndSetEvents() {
-    try {
-      const data = await fetchData();
-      setEvents(data);
-      console.log(data);
-    } catch (error) {
-      console.error("Error setting events:", error);
+    async function fetchAndSetEvents() {
+      try {
+        const data = await fetchData();
+        setEvents(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error setting events:", error);
+      }
     }
-  }
 
-  fetchAndSetEvents();
-}, []);
+    fetchAndSetEvents();
+  }, []);
 
-   if (events.length === 0) {
+  if (events.length === 0) {
     // Data is not loaded yet
     return <div>Loading...</div>; // You can replace this with a loading indicator
   }
 
-  
-if (events.length !== 0) {
-  return (
-    <>
-      <Grid templateColumns="repeat(15, 1fr)" gap={5} mt={5} mr={10}>
-        <GridItem colSpan={1}></GridItem>
-        {weekdays.map((date, index) => (
-          <GridItem colSpan={2} key={index} mb={4}>
-            <Center display={"flex"} gap={2}>
-              <Text fontWeight={"bold"} fontSize={20}>
-                {date.toLocaleDateString("en-GB", { weekday: "short" })}
-              </Text>
-              <Text fontSize={12} color={"gray.600"}>
-                {date.toLocaleDateString("en-GB", {
-                  day: "2-digit",
-                  month: "2-digit",
-                })}
-              </Text>
-            </Center>
-          </GridItem>
-        ))}
-      </Grid>
-      <Grid templateColumns="repeat(15, 1fr)" gap={5} h={500} mr={10}>
-        <GridItem colSpan={1}>
-          <VStack justify="flex-start" spacing={6}>
-            {[...Array(15)].map((_, index) => (
-              <Box key={index} h={54}>
-                {`${8 + index}:00`}
-              </Box>
-            ))}
-          </VStack>
-        </GridItem>
-        {weekdays.map((date, index) => (
-          <GridItem colSpan={2} key={index}>
-            <Flex flexDirection={"column"}>
-              {[...Array(14)].map((_, i) => (
-                <Box bg={bgWeekDay} key={i} h={79}>
-                  {events.map((event, z) => {
-                    const startDate = new Date(event.DateTimeStart);
-                    const endDate = new Date(event.DateTimeEnd);
-                    if (
-                      startDate.getDay() === date.getDay() &&
-                      startDate.getHours() <= 8 + i &&
-                      endDate.getHours() > 8 + i
-                    ) {
-                      return (
-                        <Flex
-                          key={z}
-                          h={"100%"}
-                          bg={"gray.700"}
-                          flexDirection={"column"}
-                          justifyContent={"center"}
-                        >
-                          <Text
-                            fontSize={10}
-                            color={"gray.400"}
-                            height={"20%"}
-                            pl={2}
-                          >
-                            {startDate.getHours()}:00
-                          </Text>
-                          <Center
-                            height={"60%"}
-                            display="flex"
-                            flexDirection="column"
-                          >
-                            <Text fontWeight={"bold"} fontSize={15}>
-                              {event.Title}
-                            </Text>
-                            <Text color={"gray.400"} fontSize={10}>
-                              {event.Description}
-                            </Text>
-                          </Center>
-                          <Text
-                            fontSize={10}
-                            color={"gray.400"}
-                            height={"20%"}
-                            textAlign={"right"}
-                            pr={2}
-                          >
-                            {endDate.getHours()}:00
-                          </Text>
-                        </Flex>
-                      );
-                    } else {
-                      return null;
-                    }
+  if (events.length !== 0) {
+    return (
+      <>
+        <Grid templateColumns="repeat(15, 1fr)" gap={5} mt={5} mr={10}>
+          <GridItem colSpan={1}></GridItem>
+          {weekdays.map((date, index) => (
+            <GridItem colSpan={2} key={index} mb={4}>
+              <Center display={"flex"} gap={2}>
+                <Text fontWeight={"bold"} fontSize={20}>
+                  {date.toLocaleDateString("en-GB", { weekday: "short" })}
+                </Text>
+                <Text fontSize={12} color={"gray.600"}>
+                  {date.toLocaleDateString("en-GB", {
+                    day: "2-digit",
+                    month: "2-digit",
                   })}
+                </Text>
+              </Center>
+            </GridItem>
+          ))}
+        </Grid>
+        <Grid templateColumns="repeat(15, 1fr)" gap={5} h={500} mr={10}>
+          <GridItem colSpan={1}>
+            <VStack justify="flex-start" spacing={6}>
+              {[...Array(15)].map((_, index) => (
+                <Box key={index} h={54}>
+                  {`${8 + index}:00`}
                 </Box>
               ))}
-            </Flex>
+            </VStack>
           </GridItem>
-        ))}
-      </Grid>
-    </>
-  );
-}
+          {weekdays.map((date, index) => (
+            <GridItem colSpan={2} key={index}>
+              <Flex flexDirection={"column"}>
+                {[...Array(14)].map((_, i) => (
+                  <Box bg={bgWeekDay} key={i} h={79}>
+                    {events.map((event, z) => {
+                      const startDate = new Date(event.DateTimeStart);
+                      const endDate = new Date(event.DateTimeEnd);
+                      if (
+                        startDate.getDay() === date.getDay() &&
+                        startDate.getHours() <= 8 + i &&
+                        endDate.getHours() > 8 + i
+                      ) {
+                        return (
+                          <Flex
+                            onClick={() => openModal(z)}
+                            _hover={{
+                              cursor: "pointer",
+                              bg: "gray.600",
+                            }}
+                            key={z}
+                            h={"100%"}
+                            bg={"gray.700"}
+                            flexDirection={"column"}
+                            justifyContent={"center"}
+                          >
+                            <Text
+                              fontSize={10}
+                              color={"gray.400"}
+                              height={"20%"}
+                              pl={2}
+                            >
+                              {i + 8}:00
+                            </Text>
+                            <Center
+                              height={"60%"}
+                              display="flex"
+                              flexDirection="column"
+                            >
+                              <Text fontWeight={"bold"} fontSize={15}>
+                                {event.Title}
+                              </Text>
+                              <Text color={"gray.400"} fontSize={10}>
+                                {event.Description}
+                              </Text>
+                            </Center>
+                            <Text
+                              fontSize={10}
+                              color={"gray.400"}
+                              height={"20%"}
+                              textAlign={"right"}
+                              pr={2}
+                            >
+                              {i + 9}:00
+                            </Text>
+                            {openModalIndex === z && (
+                              <Modal
+                                onClose={closeModal}
+                                isOpen={openModalIndex === z}
+                              >
+                                <ModalOverlay />
+                                <ModalContent>
+                                  <ModalHeader>{event.Title}</ModalHeader>
+                                  <ModalCloseButton />
+                                  <ModalBody>{event.Description}</ModalBody>
+                                  <ModalFooter>
+                                    <Button
+                                      colorScheme="red"
+                                      mr={3}
+                                      onClick={(e) => {
+                                        e.preventDefault();
+                                        handleDeleteClick(z, e);
+                                      }}
+                                    >
+                                      Delete{" "}
+                                    </Button>
+                                    <Button onClick={closeModal}>Close</Button>
+                                  </ModalFooter>
+                                </ModalContent>
+                              </Modal>
+                            )}
+                          </Flex>
+                        );
+                      } else {
+                        return null;
+                      }
+                    })}
+                  </Box>
+                ))}
+              </Flex>
+            </GridItem>
+          ))}
+        </Grid>
+      </>
+    );
+  }
 }
--- frontend/app/components/interfaces.tsx
+++ frontend/app/components/interfaces.tsx
@@ -1,4 +1,5 @@
 export interface Event {
+    Id: number
     Type: string;
     DateTimeStart: Date;
     DateTimeEnd: Date;
